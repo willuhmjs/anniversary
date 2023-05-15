d5959ab9d2b5a9241d96ea8b34bbf76efc36aaff
William Faircloth <willuhmjs@gmail.com>
2023-05-15 19:04:35 -0400

    feat: input groups and labels

--- src/lib/Add.svelte
+++ src/lib/Add.svelte
@@ -1,30 +1,30 @@
 <script>
 	import { onMount } from 'svelte';
 
-	let selectedEmoji = '';
+	let selectedicon = '';
 	let activeButton;
 
-	function handleEmojiClick(event) {
+	function handleiconClick(event) {
 		const button = event.target;
-		selectedEmoji = button.value;
+		selectedicon = button.value;
 
 		if (activeButton) {
-			activeButton.classList.remove('activeEmoji');
+			activeButton.classList.remove('activeicon');
 		}
 
-		button.classList.add('activeEmoji');
+		button.classList.add('activeicon');
 		activeButton = button;
 	}
 
 	onMount(() => {
-		const buttons = document.querySelectorAll('.emoji-box button');
+		const buttons = document.querySelectorAll('.icon-box button');
 		buttons.forEach((button, index) => {
 			if (index === 0) {
-				button.classList.add('activeEmoji');
+				button.classList.add('activeicon');
 				activeButton = button;
-				selectedEmoji = button.value;
+				selectedicon = button.value;
 			}
-			button.addEventListener('click', handleEmojiClick);
+			button.addEventListener('click', handleiconClick);
 		});
 	});
 </script>
@@ -35,28 +35,37 @@
 
 <div class="wrapper">
 	<form>
-		<input type="text" placeholder="Name" required />
-		<input type="date" required />
-		<div class="emoji-box">
-			<button type="button" value="fa-heart"><i class="fa-solid fa-heart"></i></button>
-			<button type="button" value="fa-cake-candles"><i class="fa-solid fa-cake-candles"></i></button>
-			<button type="button" value="fa-baby"><i class="fa-solid fa-baby"></i></button>
-			<button type="button" value="fa-champagne-glasses"><i class="fa-solid fa-champagne-glasses"></i></button>
-			<button type="button" value="fa-gift"><i class="fa-solid fa-gift"></i></button>
-            <button type="button" value="fa-clock"><i class="fa-solid fa-clock"></i></button>
-            <button type="button" value="fa-graduation-cap"><i class="fa-solid fa-graduation-cap"></i></button>
-            <button type="button" value="fa-star"><i class="fa-solid fa-star"></i></button>
-            <button type="button" value="fa-plane-departure"><i class="fa-solid fa-plane-departure"></i></button>
-            <button type="button" value="fa-umbrella-beach"><i class="fa-solid fa-umbrella-beach"></i></button>  
+        <div class="input-group">
+            <label for="name">Event Name</label>
+            <input type="text" id="name" placeholder="Wedding Day" required />
 		</div>
+        <div class="input-group">
+            <label for="date">Event Date</label>
+            <input type="date" id="date" required />
+        </div>
+        <div class="input-group">
+            <label for="icon">Event icon</label>
+            <div class="icon-box">
+                <button type="button" value="fa-heart"><i class="fa-solid fa-heart"></i></button>
+                <button type="button" value="fa-cake-candles"><i class="fa-solid fa-cake-candles"></i></button>
+                <button type="button" value="fa-baby"><i class="fa-solid fa-baby"></i></button>
+                <button type="button" value="fa-champagne-glasses"><i class="fa-solid fa-champagne-glasses"></i></button>
+                <button type="button" value="fa-gift"><i class="fa-solid fa-gift"></i></button>
+                <button type="button" value="fa-clock"><i class="fa-solid fa-clock"></i></button>
+                <button type="button" value="fa-graduation-cap"><i class="fa-solid fa-graduation-cap"></i></button>
+                <button type="button" value="fa-star"><i class="fa-solid fa-star"></i></button>
+                <button type="button" value="fa-plane-departure"><i class="fa-solid fa-plane-departure"></i></button>
+                <button type="button" value="fa-umbrella-beach"><i class="fa-solid fa-umbrella-beach"></i></button>  
+            </div>
 
-		<input
-			type="hidden"
-			id="selected-emoji"
-			name="selected-emoji"
-			bind:value={selectedEmoji}
-			required
-		/>
+            <input
+                type="hidden"
+                id="selected-icon"
+                name="selected-icon"
+                bind:value={selectedicon}
+                required
+            />
+        </div>
 		<input type="submit" value="Add Anniversary" />
 	</form>
 </div>
@@ -88,6 +97,10 @@
 		max-width: 320px;
 	}
 
+    .input-group {
+        width: 100%;
+    }
+
 	input[type='submit'] {
 		background-color: #ff1744;
 		color: #fff;
@@ -96,7 +109,7 @@
 		cursor: pointer;
 	}
 
-    .emoji-box {
+    .icon-box {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(40px, 1fr));
     grid-gap: 10px;
@@ -107,7 +120,7 @@
     max-width: 320px;
   }
 
-	.emoji-box button {
+	.icon-box button {
 		padding: 5px 10px;
 		border: none;
 		background-color: transparent;
@@ -115,11 +128,11 @@
 		cursor: pointer;
 	}
 
-	.emoji-box button:hover {
+	.icon-box button:hover {
 		background-color: #f2f2f2;
 	}
 
-	:global(.activeEmoji) {
+	:global(.activeicon) {
 		background-color: #e6e6e6 !important;
 	}
 </style>
